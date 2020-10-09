807592e480d7a6d19cfad5c4a2c9e23bfb192a8e
Francois Hendriks <Francois.Hendriks-ext@dashlane.com>
2020-10-09 18:05:57 +0200

    feat(react): add accessibility rules

--- src/generator/base-configs/reactESLintConfig.ts
+++ src/generator/base-configs/reactESLintConfig.ts
@@ -6,12 +6,13 @@ export const reactESLintConfig: Linter.Config = {
       version: "detect",
     },
   },
-  extends: ["react-app"],
+  extends: ["react-app", "plugin:jsx-a11y/recommended"],
   rules: {
     "react/no-string-refs": "warn",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
   },
+  plugins: ["jsx-a11y"],
 };
 
 export const reactTypescriptESLintConfig: Linter.Config = {
@@ -20,4 +21,4 @@ export const reactTypescriptESLintConfig: Linter.Config = {
   },
 };
 
-export const reactESLintDependencies = ["eslint-plugin-react-app", "eslint-config-react-app"];
+export const reactESLintDependencies = ["eslint-plugin-react-app", "eslint-config-react-app", "eslint-plugin-jsx-a11y"];
