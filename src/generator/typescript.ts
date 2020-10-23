e6f5cc01f9b4564a28659e0538eb31c5d874bb4e
Francois Hendriks <Francois.Hendriks-ext@dashlane.com>
2020-10-23 08:48:00 +0200

    feat(ts): add ts clean config

--- src/generator/typescript.ts
+++ src/generator/typescript.ts
@@ -1,5 +1,7 @@
+import { Linter } from "eslint";
+
 import { TypescriptInfo } from "types";
-import { identity } from "utils/utility";
+import { identity, pipe } from "utils/utility";
 import { ESLintDependencyGenerator, ESLintGenerator } from "generator/types";
 import { concatConfig, concatDependencies } from "generator/generate";
 import {
@@ -9,6 +11,36 @@ import {
   typescriptTypeEslintConfig,
 } from "generator/base-configs/typescriptEslintConfig";
 import { wrapConfigInOverride } from "generator/override";
+
+const cleanConfigRules = (config: Linter.Config): Linter.Config => {
+  const { rules, ...rest } = config;
+
+  if (!rules) {
+    return { ...rest };
+  }
+
+  const newRules = Object.entries(rules)
+    .filter(([key]) => !key.includes("@typescript-eslint"))
+    .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+
+  return { rules: newRules, ...rest };
+};
+
+const cleanConfigExtendsArray = (config: Linter.Config): Linter.Config => {
+  const { extends: extendsProperty, ...rest } = config;
+
+  const extendsArray = typeof extendsProperty === "string" ? [extendsProperty] : extendsProperty;
+
+  if (!extendsArray) {
+    return { ...rest };
+  }
+
+  const newExtendsArray = extendsArray.filter((item) => !item.includes("@typescript-eslint"));
+
+  return { extends: newExtendsArray, ...rest };
+};
+
+const cleanConfigWithTSSetup = pipe(cleanConfigRules, cleanConfigExtendsArray);
 
 export const generateTypescriptESLintConfig: ESLintGenerator = (userAnswers) => {
   const baseConfig = wrapConfigInOverride(tsOverrider)(typescriptBaseEslintConfig);
@@ -21,10 +53,10 @@ export const generateTypescriptESLintConfig: ESLintGenerator = (userAnswers) => 
       return identity;
 
     case TypescriptInfo.WithTypeChecking:
-      return concatConfig(typeConfig);
+      return pipe(concatConfig(typeConfig), cleanConfigWithTSSetup);
 
     case TypescriptInfo.NoTypeChecking:
-      return concatConfig(baseConfig);
+      return pipe(concatConfig(baseConfig), cleanConfigWithTSSetup);
   }
 };
 
