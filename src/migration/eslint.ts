a8d8171442810540f72995932340a39da10feaef
Francois Hendriks <Francois.Hendriks-ext@dashlane.com>
2020-11-06 00:46:16 +0100

    feat: upgrade performance migration

--- src/migration/eslint.ts
+++ src/migration/eslint.ts
@@ -1,8 +1,9 @@
-import { ESLint, Linter } from "eslint";
-import fs from "fs";
+import { ESLint } from "eslint";
+import { pool } from "workerpool";
+
 import { MigrationResults } from "migration/types";
 
-const getErrors = async (dirPath: string): Promise<ESLint.LintResult[]> => {
+const computeErrors = async (dirPath: string): Promise<ESLint.LintResult[]> => {
   const eslint = new ESLint({
     cwd: dirPath,
   });
@@ -11,63 +12,22 @@ const getErrors = async (dirPath: string): Promise<ESLint.LintResult[]> => {
   return results;
 };
 
-const groupErrorsByLine = (errors: Linter.LintMessage[]): Record<number, Linter.LintMessage[]> => {
-  return errors.reduce((result: Record<number, Linter.LintMessage[]>, error) => {
-    const lineNumber = error.line;
-    if (result[lineNumber] === undefined) {
-      result[lineNumber] = [];
-    }
-
-    result[lineNumber].push(error);
-    return result;
-  }, {});
-};
-
-const insertErrorIgnoreLines = (sourceLines: string[], errorsByLine: Record<number, Linter.LintMessage[]>): string[] =>
-  ((Object.entries(errorsByLine) as unknown) as [number, Linter.LintMessage[]][])
-    .sort(([lineA], [lineB]) => lineB - lineA)
-    .reduce((source, [lineNumber, errors]) => {
-      const mappedErrors = errors
-        .filter((error) => error.severity === 2 && error.fix === undefined)
-        .map((error) => error.ruleId);
-
-      if (mappedErrors.length === 0) {
-        return source;
-      }
-
-      const errorRulesString = mappedErrors.join(", ");
-
-      const ignoreString = `// eslint-disable-next-line ${errorRulesString}`;
-
-      const indentation = /^\s*/.exec(source[lineNumber - 1])?.[0] ?? "";
-
-      source.splice(lineNumber - 1, 0, indentation + ignoreString);
-
-      return source;
-    }, sourceLines);
-
-const insertIgnoreLinesInFile = (filePath: string, fileErrors: Linter.LintMessage[]) => {
-  const fileSource = fs.readFileSync(filePath).toString().split("\n");
-
-  const errorsByLine = groupErrorsByLine(fileErrors);
-
-  const newSource = insertErrorIgnoreLines(fileSource, errorsByLine).join("\n");
-
-  fs.writeFileSync(filePath, newSource);
-};
-
 export const migrateProject = async (dirPath: string): Promise<MigrationResults> => {
-  const errors = await getErrors(dirPath);
+  const errors = await computeErrors(dirPath);
+  const workerPool = pool(__dirname + "/insertIgnoreLinesScript.js");
+
+  const promises = errors.map((error) =>
+    workerPool.exec("insertIgnoreLinesInFile", [error.filePath, error.messages]).catch(console.log)
+  );
+
+  await Promise.all(promises);
+  await workerPool.terminate();
 
   return errors.reduce(
-    (results: MigrationResults, error) => {
-      insertIgnoreLinesInFile(error.filePath, error.messages);
-
-      return {
-        errorCount: results.errorCount + error.errorCount,
-        fixableErrorCount: results.fixableErrorCount + error.fixableErrorCount,
-      };
-    },
+    (results: MigrationResults, error) => ({
+      errorCount: results.errorCount + error.errorCount,
+      fixableErrorCount: results.fixableErrorCount + error.fixableErrorCount,
+    }),
     {
       errorCount: 0,
       fixableErrorCount: 0,
---
+++ src/migration/insertIgnoreLinesScript.ts
@@ -0,0 +1,54 @@
+import { Linter } from "eslint";
+import { readFileSync, writeFileSync } from "fs";
+import { worker } from "workerpool";
+
+const groupErrorsByLine = (errors: Linter.LintMessage[]): Record<number, Linter.LintMessage[]> => {
+  return errors.reduce((result: Record<number, Linter.LintMessage[]>, error) => {
+    const lineNumber = error.line;
+    if (result[lineNumber] === undefined) {
+      result[lineNumber] = [];
+    }
+
+    result[lineNumber].push(error);
+
+    return result;
+  }, {});
+};
+
+const insertErrorIgnoreLines = (sourceLines: string[], errorsByLine: Record<number, Linter.LintMessage[]>): string[] =>
+  ((Object.entries(errorsByLine) as unknown) as [number, Linter.LintMessage[]][])
+    .sort(([lineA], [lineB]) => lineB - lineA)
+    .reduce((source, [lineNumber, errors]) => {
+      const mappedErrors = errors
+        .filter((error) => error.severity === 2 && error.fix === undefined)
+        .map((error) => error.ruleId);
+
+      if (mappedErrors.length === 0) {
+        return source;
+      }
+
+      const errorRulesString = mappedErrors.join(", ");
+
+      const ignoreString = `// eslint-disable-next-line ${errorRulesString}`;
+
+      const indentation = /^\s*/.exec(source[lineNumber - 1])?.[0] ?? "";
+
+      source.splice(lineNumber - 1, 0, indentation + ignoreString);
+
+      return source;
+    }, sourceLines);
+
+const insertIgnoreLinesInFile = (filePath: string, fileErrors: Linter.LintMessage[]) => {
+  const fileSource = readFileSync(filePath).toString().split("\n");
+
+  const errorsByLine = groupErrorsByLine(fileErrors);
+
+  const newSource = insertErrorIgnoreLines(fileSource, errorsByLine).join("\n");
+
+  writeFileSync(filePath, newSource);
+};
+
+// create a worker and register public functions
+worker({
+  insertIgnoreLinesInFile,
+});
