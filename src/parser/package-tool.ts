6e99c466c70d043598f41a8a8749d50f07947109
fargito <fr.farge96@gmail.com>
2022-05-16 18:44:38 +0200

    feat: add support for yarn berry

--- src/parser/package-tool.ts
+++ src/parser/package-tool.ts
@@ -4,8 +4,13 @@ import path from "path";
 export enum PackageTool {
   NPM = "NPM",
   YARN = "YARN",
+  YARN_BERRY = "YARN_BERRY",
 }
 
 export function getPackageTool(currentDirPath: string = process.cwd()): PackageTool {
-  return fs.existsSync(path.resolve(currentDirPath, "package-lock.json")) ? PackageTool.NPM : PackageTool.YARN;
+  return fs.existsSync(path.resolve(currentDirPath, "package-lock.json"))
+    ? PackageTool.NPM
+    : fs.existsSync(path.resolve(currentDirPath, ".yarnrc.yml"))
+    ? PackageTool.YARN_BERRY
+    : PackageTool.YARN;
 }
